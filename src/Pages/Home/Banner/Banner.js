071042f3119004a7d54456ce87cdbe6be88b48b9
abudaudhossain <abudaudhossainsumon@gmail.com>
2022-01-06 12:01:26 +0600

    add bg video

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Button, Col, Container } from 'react-bootstrap';
 import "./banner.css"
+import bg_video from "./bg_video.mp4";
+
 
 const Banner = () => {
     return (
         <div className="banner">
+            <video autoPlay muted loop id="myVideo">
+                <source src={bg_video} type="video/mp4"/>
+            </video>
             <Container>
                 <Col lg={5} md={7} className="banner-text">
                     <h1>SD TRAVEl</h1>
